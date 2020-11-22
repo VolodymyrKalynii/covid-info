875f3ab962d19823a7016888d69652615e9f72a9
Volodymyr <v.kalynii@gmail.com>
2020-11-22 23:44:12 +0200

    CHANGE PORT

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ http.createServer(function(request, response){
     response.setHeader("Content-Type", "text/html; charset=utf-8;");
     response.write("<h2>hello world</h2>");
     response.end();
-}).listen(5000);
+}).listen(process.env.PORT);
 
 // const workWithData = (country) => {
 //
