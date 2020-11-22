2173db73c89f386b9e8eda6a52d3a8d90bae5fbe
Volodymyr <v.kalynii@gmail.com>
2020-11-22 23:23:47 +0200

    cha to

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const oneDayMs = 86400000; //number of milliseconds in a day
 const cacheDataLifeTime = oneDayMs * 2;
 const timeToMakeRequest = 1000 * 60 * 5;
-const botToken = Buffer.from('eG94Yi0zNjczMzI2MzcxNTMtMTQ5NjQyOTg0MTAxNS1zV0JxVXFOQWRPRjUwb3oxVnVSM056YTU=', 'base64').toString();
+const botToken = Buffer.from('eG94Yi0zNjczMzI2MzcxNTMtMTQ5NjQyOTg0MTAxNS13b3lzeE96R05GVVJERm5rY3Z1emZiWmw=', 'base64').toString();
 
 module.exports = {
     cacheDataLifeTime,
