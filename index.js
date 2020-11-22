819d1017e246ef88c019dcddb34b11d6cd816399
Volodymyr <v.kalynii@gmail.com>
2020-11-22 23:04:00 +0200

    Add server

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ initCron(rtm);
 
 http.createServer(function(request, response){
 
-    response.end("Hello world!");
+    response.setHeader("UserId", 12);
+    response.setHeader("Content-Type", "text/html; charset=utf-8;");
+    response.write("<h2>hello world</h2>");
+    response.end();
 }).listen(5000);
 
 // const workWithData = (country) => {
