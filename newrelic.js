20c07ad79feaab21481f3a05dc00e380fa630b7c
Volodymyr <v.kalynii@gmail.com>
2020-11-24 20:16:39 +0200

    add newrelic

--- newrelic.js
+++ newrelic.js
@@ -1,11 +1,11 @@
-'use strict'
+'use strict';
 /**
  * New Relic agent configuration.
  *
  * See lib/config/default.js in the agent distribution for a more complete
  * description of configuration variables and their potential values.
  */
-exports.config = {
+module.exports.config = {
   /**
    * Array of application names.
    */
