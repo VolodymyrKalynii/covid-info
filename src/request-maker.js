1cf5ff811b455d06f69ab1d7bbb99197ca26c187
Volodymyr <v.kalynii@gmail.com>
2021-01-28 21:58:17 +0200

    Added reset request

--- src/request-maker.js
+++ src/request-maker.js
@@ -20,7 +20,7 @@ const makeRequest = (callback, force) => fetch(apiLink)
         const {country, hasNewData} = checkNewData(body, force);
         console.log('hasNewData big', hasNewData);
         if (hasNewData) {
-            saveNewDataAction(country, callback);
+            saveNewDataAction(country, callback, force);
 
         } else {
             interval = setInterval(() => {
@@ -30,7 +30,7 @@ const makeRequest = (callback, force) => fetch(apiLink)
                         const {country, hasNewData} = checkNewData(body);
                         console.log('hasNewData small', hasNewData);
                         if (hasNewData) {
-                            saveNewDataAction(country, callback);
+                            saveNewDataAction(country, callback, force);
                         }
                     });
             }, timeToMakeRequest);
@@ -53,7 +53,6 @@ const checkNewData = (newData, force = false) => {
     }
 
 
-
     const oldCachedData = cache.get('parsedBody');
 
     return { // добавив провірку на 0, бо якогось фіга, зранку апі вертає нульові значення
@@ -66,34 +65,47 @@ const checkNewData = (newData, force = false) => {
  *
  * @param country
  * @param {Function} callback
+ * @param {boolean} force
  */
-const saveNewDataAction = (country, callback) => {
+const saveNewDataAction = (country, callback, force = false) => {
     clearInterval(interval);
 
     cache.put('parsedBody', country, cacheDataLifeTime);
 
     if (callback)
-        workWithData(country, callback)
+        workWithData(country, callback, force)
 };
 
 /**
  *
  * @param country
  * @param {Function} callback
+ * @param {boolean} force
  */
-const workWithData = (country, callback) => {
+const workWithData = (country, callback, force) => {
     console.log('parsedBody', country);
 
     const {confirmed, deaths, recovered, delta_confirmed, delta_deaths, delta_recovered} = country;
-    const resp = 'Прийшли нові данні за *' + currentDate + '*\n' +
-        'Нових випадків: *' + delta_confirmed + '*\n' +
-        'Одужали за добу: *' + delta_recovered + '*\n' +
-        'Летальних випадків за добу: *' + delta_deaths + '*\n' +
-        '___\n' +
-        'Всього випадків: *' + confirmed + '*\n' +
-        'Всього одужало: *' + recovered + '*\n' +
-        'Всього летальних випадків: *' + deaths + '*\n' +
-        '<@UATK7HW12>' + '<@UAW8J3TQW>';
+
+
+    const resp = !force
+        ? 'Прийшли нові данні за *' + currentDate + '*\n' +
+            'Нових випадків: *' + delta_confirmed + '*\n' +
+            'Одужали за добу: *' + delta_recovered + '*\n' +
+            'Летальних випадків за добу: *' + delta_deaths + '*\n' +
+            '___\n' +
+            'Всього випадків: *' + confirmed + '*\n' +
+            'Всього одужало: *' + recovered + '*\n' +
+            'Всього летальних випадків: *' + deaths + '*\n' +
+            '<@UATK7HW12>' + '<@UAW8J3TQW>'
+        : 'Прийшли нові данні за *' + currentDate + '*\n' +
+            'Нових випадків: *' + delta_confirmed + '*\n' +
+            'Одужали за добу: *' + delta_recovered + '*\n' +
+            'Летальних випадків за добу: *' + delta_deaths + '*\n' +
+            '___\n' +
+            'Всього випадків: *' + confirmed + '*\n' +
+            'Всього одужало: *' + recovered + '*\n' +
+            'Всього летальних випадків: *' + deaths + '*\n';
     // '';
 
     callback(resp);
